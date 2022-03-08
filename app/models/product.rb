0ead0afe06e0c6d6fbbd7e416039a9ac3390529c
chirag <chirag.r@simformsolutions.com>
2022-03-08 15:05:48 +0530

    Product stock and price Validation changed.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
     validates :name, presence: true, length:{minimum:2, maximum:100}
     validates :description, presence: true,length:{minimum:2, maximum:500}
-    validates :stock, presence: true,length: { is: 5 }
-    validates :price, presence: true,length: { is: 8 }
+    validates :stock, presence: true,length:{minimum:2, maximum:8}
+    validates :price, presence: true,length:{minimum:2, maximum:8}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_072515) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_08_081637) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
